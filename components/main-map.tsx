c8bf9cf0c1b34cb0a014221ed13b09b949e126ba
Roger French <rogerf76@gmail.com>
2025-04-01 16:10:57 +1100

    Add Mapbox

---
+++ components/main-map.tsx
@@ -0,0 +1,23 @@
+'use client';
+import React from 'react';
+
+// Using Mapbox
+import Map, {Marker} from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export const MainMapComponent = () => {
+    return (
+        <Map
+            initialViewState={{
+                latitude: 37.8,
+                longitude: -122.4,
+                zoom: 14
+            }}
+            style={{ width: 800, height: 600 }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        >
+            <Marker longitude={-122.4} latitude={37.8} color="red" />
+        </Map>
+    );
+};
